9190bb56c1543f6c08dae3f95aa6bc1033096360
Sherfin Shamsudeen <sherfin94@gmail.com>
2016-09-14 13:55:20 +0530

    Implement static version
    This commit fully implements the static version of the app ui
    with the search bar, and table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProductTable from './ProductTable'
+import SearchBar from'./SearchBar'
+import FilterableProductTable from './FilterableProductTable'
 
 class App extends Component {
   render() {
+    var PRODUCTS = [
+      {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+      {category: 'Electronics', price: '$10.99', stocked: true, name: 'Light-Bulb'}
+    ];
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +20,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <FilterableProductTable products={PRODUCTS} />
       </div>
     );
   }
